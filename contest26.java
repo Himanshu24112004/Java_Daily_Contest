9e603837f5c5b6960ecad34853049145cb880086
Himanshu Kumar <himayu0001@gmail.com>
2026-04-13 22:00:22 +0530

    day 26 contest : error fix

--- contest26.java
+++ contest26.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+ class Solution {
     
     long baseEnergy;
     
@@ -20,7 +20,7 @@ public class Solution {
     double add(double x,double y){
         return x+y+baseEnergy;
     }
-    
+} 
 public class contest26 {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
