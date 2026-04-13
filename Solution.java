741a6b05cac8f6877f425e578a1fc9a10316e4a0
Himanshu Kumar <himayu0001@gmail.com>
2026-04-13 21:58:43 +0530

    day 26 contest : error fixed

--- Solution.java
+++ Solution.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class contest26 {
+public class Solution {
     
     long baseEnergy;
     
@@ -21,7 +21,7 @@ public class contest26 {
         return x+y+baseEnergy;
     }
     
-
+public class contest26 {
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
