1574a7d8d213ebca28033c62639e0f8d20709fd5
Himanshu Kumar <himayu0001@gmail.com>
2026-06-03 10:38:07 +0530

    modified contest61_q1

--- contest61_q1.java
+++ contest61_q1.java
@@ -5,16 +5,21 @@ public class contest61_q1 {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
         Scanner sc = new Scanner(System.in);
         long n = sc.nextLong();
-        long sum = 0;
         boolean isSum = false;
-        
-        for(long i=0; i<=n;i++){
-            sum += i;
-            if(sum==n){
-                isSum = true;
-                break;
+
+        for(int i=0; i<=n;i++){
+            long sum = 0;
+            for(int j=0; j<n;j++){
+                sum += j;
+                if(sum==n){
+                    isSum = true;
+                    break;
+                }
+                if(sum>n)
+                    break;
             }
         }
         if(isSum){
